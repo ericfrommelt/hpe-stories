2f333e272c37fb7e5d7d186a60c63a5cb4ea914c
Eric Frommelt <ericfrommelt@gmail.com>
2021-07-14 14:32:33 -0400

    link edit

--- src/pages/index.js
+++ src/pages/index.js
@@ -50,7 +50,7 @@ const IndexPage = (props) => (
     <div className={style.flex__wrap}>
       <section className={style.intro}>
         <h1 className={style.intro__title}>Editorial experiences for HPE</h1>
-        <p classname={style.intro__copy}>Design and development by <a className={style.home__link} href="http://www.ericfrommelt.com">Eric Frommelt</a></p>
+        <p classname={style.intro__copy}>Design and development by <a className={style.home__link} href="http://www.alphanumeric.io">Eric Frommelt</a></p>
         <p className={style.tech}>Gatsby, CSS Modules, GSAP, visx</p>
         <div className={style.colorBar}></div>
       </section> 
