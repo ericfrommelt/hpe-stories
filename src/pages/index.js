e4fe62a8fa16dda1e16c1d114d89393a06498845
Eric Frommelt <ericfrommelt@gmail.com>
2022-08-21 11:29:26 -0700

    edit url

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,12 @@
-import React from 'react'
-import { graphql } from 'gatsby'
-import { Link } from 'gatsby'
-import { Helmet } from 'react-helmet'
-import Layout from '../components/layout'
-import BackgroundImage from 'gatsby-background-image'
-import style from './index.module.css'
+import React from "react"
+import { graphql } from "gatsby"
+import { Link } from "gatsby"
+import { Helmet } from "react-helmet"
+import Layout from "../components/layout"
+import BackgroundImage from "gatsby-background-image"
+import style from "./index.module.css"
 
-const IndexPage = (props) => (
+const IndexPage = props => (
   <Layout>
     <Helmet>
       <meta charSet="utf-8" />
@@ -16,8 +16,13 @@ const IndexPage = (props) => (
       <div className={style.story}>
         <div className={style.column}>
           <Link to="/cloud-cliff/">
-            <BackgroundImage className={style.bgImage} fluid={props.data.engineImage.childImageSharp.fluid}>
-              <h1 className={style.story__titleCloudCliff}>Master the Cloud Cliff</h1>
+            <BackgroundImage
+              className={style.bgImage}
+              fluid={props.data.engineImage.childImageSharp.fluid}
+            >
+              <h1 className={style.story__titleCloudCliff}>
+                Master the Cloud Cliff
+              </h1>
               <div className={style.overlay}></div>
             </BackgroundImage>
           </Link>
@@ -26,9 +31,14 @@ const IndexPage = (props) => (
       <div className={style.story}>
         <div className={style.column}>
           <Link to="/genomic-basics/">
-            <BackgroundImage className={style.bgImage} fluid={props.data.genomeImage.childImageSharp.fluid}>
-                <h1 className={style.story__titleGenomic}>Genomic Basics for CIOs</h1>
-                <div className={style.overlay}></div>
+            <BackgroundImage
+              className={style.bgImage}
+              fluid={props.data.genomeImage.childImageSharp.fluid}
+            >
+              <h1 className={style.story__titleGenomic}>
+                Genomic Basics for CIOs
+              </h1>
+              <div className={style.overlay}></div>
             </BackgroundImage>
           </Link>
         </div>
@@ -36,9 +46,14 @@ const IndexPage = (props) => (
       <div className={style.story}>
         <div className={style.column}>
           <Link to="/cybercrime/">
-            <BackgroundImage className={style.bgImage} fluid={props.data.hackerImage.childImageSharp.fluid}>
-                <h1 className={style.story__title}>Cybercrime: The Long Road to Recovery</h1>
-                <div className={style.overlay}></div>
+            <BackgroundImage
+              className={style.bgImage}
+              fluid={props.data.hackerImage.childImageSharp.fluid}
+            >
+              <h1 className={style.story__title}>
+                Cybercrime: The Long Road to Recovery
+              </h1>
+              <div className={style.overlay}></div>
             </BackgroundImage>
           </Link>
         </div>
@@ -50,27 +65,42 @@ const IndexPage = (props) => (
     <div className={style.flex__wrap}>
       <section className={style.intro}>
         <h1 className={style.intro__title}>Editorial experiences for HPE</h1>
-        <p classname={style.intro__copy}>Design and development by <a className={style.home__link} href="http://www.ericfrommelt.design">Eric Frommelt</a></p>
+        <p classname={style.intro__copy}>
+          Design and development by{" "}
+          <a className={style.home__link} href="https://www.alphanumeric.io">
+            Eric Frommelt
+          </a>
+        </p>
         <p className={style.tech}>Gatsby, CSS Modules, GSAP, visx</p>
         <div className={style.colorBar}></div>
-      </section> 
+      </section>
       <section className={style.footer}>
         <div className={style.backWrap}>
           <div className={style.back}>
             <div className={style.back__arrow}>
               <svg
-              aria-labelledby="backarrow"
-              id="backarrow"
-              role="link"
-              xmlns="http://www.w3.org/2000/svg" viewBox="0 0 22.87 15.83">
+                aria-labelledby="backarrow"
+                id="backarrow"
+                role="link"
+                xmlns="http://www.w3.org/2000/svg"
+                viewBox="0 0 22.87 15.83"
+              >
                 <title>backarrow</title>
-                <g id="backarrow" data-name="backarrow" fill="none" stroke="#fff" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="2px">
-                  <path d="M21.87 7.91h-20M8.33 14.83L1.41 7.91 8.33 1"/>
+                <g
+                  id="backarrow"
+                  data-name="backarrow"
+                  fill="none"
+                  stroke="#fff"
+                  strokeLinecap="round"
+                  strokeMiterlimit="10"
+                  strokeWidth="2px"
+                >
+                  <path d="M21.87 7.91h-20M8.33 14.83L1.41 7.91 8.33 1" />
                 </g>
               </svg>
             </div>
             <div className={style.back__alphanumeric}>
-              <a className={style.link__a} href="https://www.ericfrommelt.design/">
+              <a className={style.link__a} href="https://www.alphanumeric.io/">
                 <p>Back</p>
               </a>
             </div>
